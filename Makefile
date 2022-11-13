aee5cf4bff7b00ed47281546071b4a1ad6e85f7d
Arghya Biswas <arghya.ialoy.com>
2022-11-13 12:16:30 +0530

    Update the actual code for the project
    Signed-off-by: Arghya Biswas <arghya.ialoy.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
 	@rm -rf src/build-*
 	@cd src && make
-	@cp src/build-uno/src.hex tree_light.hex
+	@cp src/build-uno/src_.hex tree_light.hex
 	@rm -rf src/build-*
 
 
@@ -11,4 +11,4 @@ clean:
 
 
 burn:
-	@sudo avrdude -v -Cavrdude.conf -patmega328p -carduino -P/dev/ttyUSB1 -b57600 -D -Uflash:w:tree_light.hex:i
+	@sudo avrdude -v -Cavrdude.conf -patmega328p -carduino -P/dev/ttyUSB0 -b57600 -D -Uflash:w:tree_light.hex:i
--- src/tree_light.ino
+++ src/tree_light.ino
@@ -1,26 +1,44 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+#define VARIAC_PIN   A2
+#define SIPO_CLK     6
+#define SIPO_LATCH   7
+#define SIPO_DATA    8
 
-  This example code is in the public domain.
- */
+uint32_t SIPO_dataset; //XXXX_XXXX__1100_0000__0000_0000__0000_0000
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-#include <Arduino.h>
 
-int led = 13;
-
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
+void setup()
+{
+    pinMode(SIPO_DATA, OUTPUT);
+    pinMode(SIPO_CLK, OUTPUT);
+    pinMode(SIPO_LATCH, OUTPUT);
+    pinMode(VARIAC_PIN, INPUT);
+    digitalWrite(SIPO_DATA, LOW);
+    digitalWrite(SIPO_CLK, LOW);
+    digitalWrite(SIPO_LATCH, HIGH);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
+
+void loop()
+{
+    int variable_value = analogRead(VARIAC_PIN);
+    if(variable_value < 10)
+    {
+        SIPO_dataset = 0xFFFFFFFF;
+    }
+    else
+    {
+        variable_value = map(variable_value, 10, 1023, 10, 1000);
+        SIPO_dataset = random(0xFF000000, 0xFFFFFFFF);
+        delay(variable_value);
+    }
+
+    for(int i = 0; i < 24; i++)
+    {
+        digitalWrite(SIPO_DATA, !!(SIPO_dataset & ((uint32_t)1 << i)));
+
+        digitalWrite(SIPO_CLK, HIGH);
+        digitalWrite(SIPO_CLK, LOW);
+    }
+    digitalWrite(SIPO_LATCH, LOW);
+    digitalWrite(SIPO_LATCH, HIGH);
 }
